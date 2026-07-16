7b0e49dfbb7d23205ebd4ea867975070cd683003
Ayush Raj <mailtoayushh@gmail.com>
2026-07-17 01:51:17 +0530

    Agent Creation

---
+++ agent/agent.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+load_dotenv()
+from rich import print
+from langchain.agents import create_agent
+from langchain_mistralai import ChatMistralAI
+from tools.calculator import calculator
+from tools.date_and_time import get_date
+from tools.dice_and_coin import dice,coin
+from tools.middle import middleman
+from tools.password_generator import password_generator
+from tools.UUID_generator import uuid_generator
+
+model = ChatMistralAI(model='mistral-small-2506')
+tools =[calculator,get_date,dice,coin,password_generator,uuid_generator]
+agent = create_agent(model=model,tools=tools,system_prompt="""You are a productivity assistant.
+
+You have access to tools.
+
+Whenever a user's request can be solved using a tool, you MUST use the appropriate tool.
+
+Never perform calculations yourself.
+
+Always use the calculator tool for arithmetic.""")
+
+
+while True:
+    query = input("You : ")
+    if query == 'exit':
+        break
+    result = agent.invoke(
+
+        {"messages":[{"role":"user","content": query}]}
+    )
+
+    print(f"Bot : {result['messages'][-1].content}")
